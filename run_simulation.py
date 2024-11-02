a07db929f4e683f785cb4a546bf2eb833a536466
Vasilii Ramadanov <finimpres@gmail.com>
2024-11-03 00:02:16 +0300

    Refactor YAML configs
    - reuse config files with !include
    - remove unused code

--- run_simulation.py
+++ run_simulation.py
@@ -15,14 +15,24 @@ parser.add_argument('--set', action='append', nargs=2, metavar=('key', 'value'),
 parser.add_argument('scenario', type=str, help='a scenario to run')
 
 
+def include_constructor(loader, node):
+    file_names = loader.construct_sequence(node)
+    merged_data = {}
+
+    for file_name in file_names:
+        with open(file_name, 'r') as f:
+            data = yaml.safe_load(f)
+            merged_data.update(data)
+
+    return merged_data
+
+
+yaml.SafeLoader.add_constructor('!include', include_constructor)
+
+
 def load_config(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
-
-
-def save_config(file_path, config):
-    with open(file_path, 'w') as file:
-        yaml.safe_dump(config, file)
 
 
 if __name__ == '__main__':
@@ -36,7 +46,7 @@ if __name__ == '__main__':
             for entry in key.split('.'):
                 field = field[entry]
             field = value
-
+    print(f"logging config: {config}")
     logging.basicConfig(**config['logging'])
     for module in ['matplotlib', 'PIL']:
         logger = logging.getLogger(module)
